daf876744844d987a1d1fbcd97f86033ae86a8c3
mathengejob <jobmathenge90@gmail.com>
2021-12-10 08:52:07 +0300

    Commit1

--- pdfconverter/main.py
+++
@@ -1,9 +0,0 @@
-from pdf2docx import Converter
-
-pdf_file = 'input/resume.pdf'
-docx_file = 'output/resume.docx'
-
-# convert pdf to docx
-cv = Converter(pdf_file)
-cv.convert(docx_file)      # all pages by default
-cv.close()
